e573073c988e05184fa9759f42395ed9360aaac3
viscosenpai <kama.hiroki@gmail.com>
2021-12-28 01:01:17 +0900

    [add] modified button, multi selections

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<bool> isSelected = [true, false, false, false, false, false];
+  bool isModified = false;
+  List<int> isSelected = [0, 0, 0, 0, 0, 0];
   List<IconData> iconList = [
     Icons.ac_unit,
     Icons.call,
@@ -46,6 +47,14 @@ class _MyHomePageState extends State<MyHomePage> {
     'label5',
     'label6',
   ];
+  List<List<String>> valueList = [
+    ['value1', 'value2', 'value3'],
+    ['value1', 'value2', 'value3'],
+    ['value1', 'value2', 'value3'],
+    ['value1', 'value2', 'value3'],
+    ['value1', 'value2', 'value3'],
+    ['value1', 'value2', 'value3'],
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -53,58 +62,87 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
-        height: double.infinity,
-        width: double.infinity,
-        child: Ink(
-          color: Colors.white,
-          child: GridView.count(
-            primary: true,
-            crossAxisCount: 3,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            childAspectRatio: 1,
-            children: List.generate(isSelected.length, (index) {
-              return InkWell(
-                onTap: () {
-                  setState(() {
-                    isSelected[index] = !isSelected[index];
-                  });
-                },
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(
+                    top: 20, bottom: 20, left: 20, right: 20),
+                height: double.infinity,
+                width: double.infinity,
                 child: Ink(
-                  decoration: BoxDecoration(
-                      color:
-                          isSelected[index] ? Colors.redAccent : Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(
-                        color: Colors.redAccent,
-                        width: 3,
-                      )),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 15),
-                      Text(
-                        labelList[index],
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: isSelected[index]
-                              ? Colors.white
-                              : Colors.redAccent,
+                  color: Colors.white,
+                  child: GridView.count(
+                    primary: true,
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 1,
+                    children: List.generate(iconList.length, (index) {
+                      return InkWell(
+                        onTap: () {
+                          if (isModified) {
+                            setState(() {
+                              // isSelected[index] = !isSelected[index];
+                              if (valueList[index].length ==
+                                  ++isSelected[index]) {
+                                isSelected[index] = 0;
+                              }
+                            });
+                          }
+                        },
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              color:
+                                  isModified ? Colors.white : Colors.redAccent,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: Colors.redAccent,
+                                width: 3,
+                              )),
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 15),
+                              Text(
+                                valueList[index][isSelected[index]],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: isModified
+                                      ? Colors.redAccent
+                                      : Colors.white,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Icon(
+                                iconList[index],
+                                size: 30,
+                                color: isModified
+                                    ? Colors.redAccent
+                                    : Colors.white,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 5),
-                      Icon(iconList[index],
-                          size: 30,
-                          color: isSelected[index]
-                              ? Colors.white
-                              : Colors.redAccent),
-                    ],
+                      );
+                    }),
                   ),
                 ),
-              );
-            }),
-          ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isModified = !isModified;
+                  print(isModified);
+                });
+              },
+              child: Text('Edit'),
+            ),
+          ],
         ),
       ),
     );
